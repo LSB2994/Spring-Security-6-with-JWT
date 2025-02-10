7c8991f87ef45ba4e5125e8879874b34555714b0
LSB7 <lengsambthsb2004@gamil.com>
2025-02-10 16:20:25 +0700

    final

---
+++ src/main/java/com/example/authsender/config/OpenAipConfiguration.java
@@ -0,0 +1,16 @@
+package com.example.authsender.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAipConfiguration {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
